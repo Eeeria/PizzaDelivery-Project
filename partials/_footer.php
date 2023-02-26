19f28040e424df45b789dcfe9cddbea96f05ceac
Chem Rill <72859741+greyCat07@users.noreply.github.com>
2023-02-27 01:42:45 +0800

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,48 @@
+</div>
+    <div class="container-fluid bg-dark text-light">
+        <div class="container">
+            <div class="row ">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 footer pt-5">
+                    <p style="font-size:20px;text-transform: uppercase;font-weight: 800;" data-aos="slide-right">Pizza Delivery Philippines</p>
+                    <p data-aos="slide-right"> Pizza delivery Philippines is a Web application that allows customers to conveniently order pizza from four popular pizza brands in the Philippines: Greenwich, Pizza Hut, Shakey's, and Yellow Cab. This app is designed to provide a hassle-free and efficient way of ordering pizza from these popular brands. With this app, customers can browse through the menus of these four pizza brands, select their desired pizza type, size, and toppings, and place their orders. The app also allows customers to track their orders in real-time, from the moment their order is placed to the moment it is delivered to their doorstep.
+  <br></p>
+
+                </div>
+                <div class="col-xl-3 pt-5 hide-girl">
+                    <span style="font-size:20px;text-transform: uppercase;font-weight: 800;" data-aos="zoom-in-left">Restaurants </span><br>
+                    <p style="margin-top:3%" data-aos="slide-up">Luzon </p>
+                    <p style="margin-top:-3%" data-aos="slide-up">Visayas </p>
+                    <p style="margin-top:-3%" data-aos="slide-up">Mindanao </p>
+                    <p style="margin-top:-3%;" data-aos="slide-up">Pizza International</p>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-4 col-sm-12 col-12 footer  pt-5">
+                    <span style="font-size:20px;text-transform: uppercase;font-weight: 800;" class="text-center" data-aos="zoom-in-left">Follow us:</span><br>
+                    <p style="margin-top:3%;margin-left:1%;" data-aos="fade-up">instagram.com/Pizzadelivery</p>
+                    <p style="margin-top:-3%;margin-left:1%;" data-aos="fade-up">facebook.com/Pizzadelivery</p>
+                    <p style="margin-top:-3%;margin-left:1%;" data-aos="fade-up">twitter.com/Pizzadelivery</p>
+                    <p style="margin-top:-3%;margin-left:1%;" data-aos="fade-up">tg.com/Pizzadelivery</p>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-4 col-sm-12 col-12 footer pt-5 pb-3">
+                    <p style="font-size:20px;text-transform: uppercase;font-weight: 800;" data-aos="slide-left">contact</p>
+                    <p data-aos="fade-up"><i class="fa fa-home" aria-hidden="true" style="font-size:20px;"></i>&nbsp; Manila, Philippines,</p>
+                    <p data-aos="fade-up"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp; Pizzadelivery@gmail.com</p>
+                    <p data-aos="fade-up"><i class="fa fa-phone" aria-hidden="true"></i>&nbsp; 09566595176</p>
+                    <p data-aos="fade-up"><i class="fa fa-print" aria-hidden="true"></i>&nbsp; 09566595176</p>
+                </div>
+            </div>
+        </div>
+        <hr style="color:rgb(0, 0, 0);width:100%;border:1px solid">
+        <div class="container mt-4 pb-4">
+            <div class="row">
+                <div class="col-12 col-xl-6 col-lg-6 col-md-6 col-sm-6 ">
+                    <p> 2023 Pizza <span style="font-weight:600">PH</span></p>
+                </div>
+                <div class="col-12 col-xl-6 col-lg-6 col-md-6 col-sm-6 icon-head">
+                    <span class="icons-2"><i class="fa fa-facebook" aria-hidden="true"></i></span>
+                    <span class="icons-3"><i class="fa fa-twitter" aria-hidden="true"></i></span>
+                    <span class="icons-1"><i class="fa fa-google-plus" aria-hidden="true"></i></span>
+                    <span class="icons"><i class="fa fa-instagram" aria-hidden="true"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
